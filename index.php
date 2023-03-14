9974df7253f67db431f8016f906adfa611ab612d
Samuelimmerso <samuelimmerso.info@gmail.com>
2023-03-15 00:48:32 +0100

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    
+<select class="form-select">
+  <option selected>Seleziona la difficoltà</option>
+  <option value="1">Low</option>
+  <option value="2">Normal</option>
+  <option value="3">Strong</option>
+</select>
+
+
+</body>
+</html>
